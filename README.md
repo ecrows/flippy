bf55c7f2879c66962fcda1f92f830ee98c636105
ecrows <evan.of.crows@gmail.com>
2018-07-27 21:38:32 -0400

    Configurable output codecs

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 # overlay-lapse
 Python script for generating videos by overlaying images.
 
-For usage information:
+usage: lapse.py [-h] [-v] [-f FPS] [-o OUTPUT] [-c CODEC] path [path ...]
 
-python lapse.py --help
+Build a fading timelapse of overlayed images.
+
+positional arguments:
+  path                  path to read image files from
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --verbose         verbose logging
+  -f FPS, --fps FPS     frames per second
+  -o OUTPUT, --output OUTPUT
+                        output file
+  -c CODEC, --codec CODEC
+                        fourcc codec
+
+Example:
+
+python3 lapse.py /some/path/faces\_64px -o ~/output.avi -f 60.0 -c XVID
+
+INFO:root:Auto-detected image dimensions as 64x64...
+INFO:root:Building fading timelapse of 24247 images in /some/path/faces\_64px...
+INFO:root:Output settings: 64x64 @ 60.0 FPS
+
--- lapse.py
+++ lapse.py
@@ -8,12 +8,19 @@ import numpy as np
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Build a fading timelapse of overlayed images.')
+parser = argparse.ArgumentParser(
+    description='Build a fading timelapse of overlayed images.')
 
-parser.add_argument('path', nargs='+', help='path to read image files from', type=str)
-parser.add_argument('-v', '--verbose', help='verbose logging', action='store_true')
-parser.add_argument('-f', '--fps', nargs=1, help='frames per second', type=float, default=[20.0])
-# TODO: Outfile
+parser.add_argument('path', nargs='+',
+                    help='path to read image files from', type=str)
+parser.add_argument('-v', '--verbose',
+                    help='verbose logging', action='store_true')
+parser.add_argument('-f', '--fps', nargs=1,
+                    help='frames per second', type=float, default=[20.0])
+parser.add_argument('-o', '--output', nargs=1,
+                    help='output file', type=str, default=['video.avi'])
+parser.add_argument('-c', '--codec', nargs=1,
+                    help='fourcc codec', type=str, default=['DIVX'])
 
 args = parser.parse_args()
 
@@ -26,7 +33,7 @@ else:
 fps = args.fps[0]
 
 # Read image path (required)
-image_path=args.path[0]
+image_path = args.path[0]
 
 extensions = ['jpg', 'jpeg', 'png', 'bmp', 'gif']
 all_files = []
@@ -34,7 +41,7 @@ all_files = []
 for extension in extensions:
     all_files.extend(glob.glob(image_path + "/*." + extension))
 
-# Use first file as template for images
+# Use first file as template for image output
 try:
     sample = cv2.imread(all_files[0], cv2.IMREAD_COLOR)
 except IndexError:
@@ -43,16 +50,17 @@ except IndexError:
 
 out_height, out_width,  = sample.shape[:2]
 
-logging.info('Auto-detected image dimensions as {}x{}...'.format(out_width, out_height))
+logging.info(
+    'Auto-detected image dimensions as {}x{}...'.format(out_width, out_height))
 
-# DIVX seems to work on Windows and Debian, TODO: add override for other codecs[0:1]
-fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+fourcc = cv2.VideoWriter_fourcc(*args.codec[0]) # See OpenCV video docs for more
 
-# TODO: Input/output dimensions and formats, auto detect size from first image
-video = cv2.VideoWriter('video.avi', fourcc, fps, (out_width, out_height))
+video = cv2.VideoWriter(args.output[0], fourcc, fps, (out_width, out_height))
 
-logging.info('Building fading timelapse of {} images in {}...'.format(len(all_files), image_path))
-logging.info('Output settings: {}x{} @ {} FPS'.format(out_width, out_height, fps))
+logging.info('Building fading timelapse of {} images in {}...'.format(
+    len(all_files), image_path))
+logging.info('Output settings: {}x{} @ {} FPS'.format(
+    out_width, out_height, fps))
 
 # Array that holds current blended image data
 master = np.zeros((out_height, out_width, 3), np.uint8)
@@ -62,19 +70,18 @@ for filename in all_files:
     image = cv2.imread(filename, cv2.IMREAD_COLOR)
     width, height = image.shape[:2]
 
-    # TODO: Ask forgiveness?
     if width != out_width or height != out_height:
-        logging.debug("Skipping invalid shaped image found in path: {}".format(filename))
+        logging.debug(
+            "Skipping invalid shaped image found in path: {}".format(filename))
         continue
-    
-    # TODO: Preview mode
-    # cv2.imshow('Combined', master)
-    # cv2.waitKey()
-    
+
     # TODO: Confirm that with very large numbers of images the precision on this is sufficient
-    master = cv2.addWeighted(master, float((count-1)/count), image, float(1.0/count), 0)
+    master = cv2.addWeighted(master, float(
+        (count-1)/count), image, float(1.0/count), 0)
     video.write(master)
-    
+
     count += 1
-    
+
+logging.info('Successfully wrote {}'.format(args.output[0]))
+
 video.release()
