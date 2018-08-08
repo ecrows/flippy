d99a280a9b7d154da84744c812b680e7190b5da3
ecrows <evan.of.crows@gmail.com>
2018-08-08 00:19:56 -0400

    Incrementing version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
     name = 'flippy',
-    version = '0.1.0',
+    version = '0.1.1',
     description = 'Build videos from image directories',
     long_description = long_description,
     long_description_content_type="text/markdown",
